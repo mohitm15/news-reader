4d015b17f0b79dec478d72f96387b2ab53c45d5d
Mohit Maroliya <mmaroliya@gmail.com>
2021-06-17 16:51:24 +0530

    Fetching data from API

--- news-reader/src/components/News/News.js
+++ news-reader/src/components/News/News.js
@@ -1,9 +1,40 @@
 import React,{Component} from 'react';
+import NewSingle from './NewSingle';
 
 class News extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state={
+            news:[],
+        };
+    }
+
+    componentDidMount() {
+        const url = 'https://newsapi.org/v2/everything?q=tesla&from=2021-05-17&sortBy=publishedAt&apiKey=fbd0ed465f3f46c5924789d3e4810180';
+
+        //fetching data
+        fetch(url)
+        .then(response=>response.json())
+        .then((data)=>{
+            this.setState({
+                news:data.articles
+            })
+        })
+        .catch(console.error)
+    }
+
+    renderItems () {
+        return this.state.news.map((item)=>(
+            <NewSingle key={item.id} item={item}/>
+        ));
+    }
+
     render() {
         return (
-            <h2>News page here</h2>
+            <ul>
+                {this.renderItems()}
+            </ul>
         );
     }
 }
